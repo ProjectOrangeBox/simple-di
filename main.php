d0a43af8e771be0f2015d02589c14805077abdcb
Don Myers <donmyers@me.com>
2020-01-17 07:57:47 -0500

    refactor

--- main.php
+++ main.php
@@ -2,11 +2,30 @@
 
 require __DIR__ . '/autoloader.php';
 
+function di($serviceName = null)
+{
+	static $di;
+
+	if (!$di) {
+		if (!is_array($serviceName)) {
+			throw new Exception('Please provide the services configuration before using the dependency injector.');
+		} else {
+			$di = new di($serviceName);
+
+			$serviceName = null;
+		}
+	}
+
+	return ($serviceName) ? $di->get($serviceName) : $di;
+}
+
+$services = require __DIR__ . '/services.php';
+
 /* set up */
-$di = new di(require __DIR__ . '/services.php');
+$di = di($services);
 
 /* test stuff */
-$config = $di->config;
+$config = di('config');
 
 $config->set('name', 'Don Myers');
 $config->set('age', 21);
